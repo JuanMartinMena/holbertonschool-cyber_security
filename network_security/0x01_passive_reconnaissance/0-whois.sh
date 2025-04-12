33613698bf5d0c6abce5c0b6cfd6468629686620
JuanMartinMena <juanmartinmenac@gmail.com>
2025-04-12 17:26:13 -0400

    Making the exact output

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F: '/^(Registrant|Admin|Tech)/ && NF>1 {gsub(/^ +| +$/, "", $1); gsub(/^ +| +$/, "", $2); print $1 "," $2}' > "$1.csv"
+whois $1 | awk -F: '/^(Registrant|Admin|Tech) (Organization|State\/Province|Country|Email)/ {gsub(/^ +| +$/, "", $2); print $1 "," $2}' > "$1.csv"

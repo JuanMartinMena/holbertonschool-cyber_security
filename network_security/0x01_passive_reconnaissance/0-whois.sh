27abbd8d3571dda4150031d7c8bc92e4073a2ae4
JuanMartinMena <juanmartinmenac@gmail.com>
2025-04-12 17:19:04 -0400

    add more details to search of awk

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F: '/^(Registrant|Admin|Tech)/ && NF>1 {gsub(/^ +| +$/, "", $1); gsub(/^ +| +$/, "", $2); print $1 "," $2}' > "$1.csv"
+whois $1 | paste -sd ' ' - | awk -F: '{gsub(/^ +| +$/, "", $1); gsub(/^ +| +$/, "", $2); if ($1 ~ /^(Registrant|Admin|Tech).* (Organization|State\/Province|Country|Email)$/) print $1 "," $2}' > "$1.csv"

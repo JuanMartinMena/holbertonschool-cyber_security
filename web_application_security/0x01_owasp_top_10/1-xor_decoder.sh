51ded72eb7c0fede325cf0a958798ebfdcea877d
JuanMartinMena <juanmartinmenac@gmail.com>
2025-05-11 18:42:33 -0400

    working on xor decoder

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "${1#\{xor\}}" | base64 -d | xxd -p | tr -d '\n' | sed 's/../\\x&/g' | xargs -0 printf "%b" | \
-    perl -pe 's/(.)/chr(ord($1)^0x95)/ge'
+echo "${1#\{xor\}}" | base64 -d | perl -pe 's/(.)/chr(ord($1)^0x95)/ge'

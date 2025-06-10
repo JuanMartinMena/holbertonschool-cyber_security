121d4f089749a2cd9bcdf7d900eaa8153a4fd6c5
JuanMartinMena <juanmartinmenac@gmail.com>
2025-06-09 20:47:33 -0400

    trying to fix output

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -2,6 +2,8 @@
 HASH_FILE="$1"
 WORDLIST="/usr/share/wordlists/rockyou.txt"
 
+# Ejecuta John con la wordlist sobre el archivo de hashes
 john --wordlist="$WORDLIST" --format=raw-md5 "$HASH_FILE"
 
-john --show --format=raw-md5 "$HASH_FILE" | cut -d: -f2 > 4-password.txt
+# Extrae solo las contraseñas, elimina líneas vacías y limita a 2 líneas
+john --show --format=raw-md5 "$HASH_FILE" | cut -d: -f2 | sed '/^$/d' | head -n 2 > 4-password.txt

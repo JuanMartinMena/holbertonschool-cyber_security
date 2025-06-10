977450c74be528cdc93002faf8570fef65a54647
JuanMartinMena <juanmartinmenac@gmail.com>
2025-06-09 20:49:42 -0400

    reduce size of the script

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-HASH_FILE="$1"
-WORDLIST="/usr/share/wordlists/rockyou.txt"
-
-# Ejecuta John con la wordlist sobre el archivo de hashes
-john --wordlist="$WORDLIST" --format=raw-md5 "$HASH_FILE"
-
-# Extrae solo las contraseñas, elimina líneas vacías y limita a 2 líneas
-john --show --format=raw-md5 "$HASH_FILE" | cut -d: -f2 | sed '/^$/d' | head -n 2 > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1"; john --show --format=raw-md5 "$1" | cut -d: -f2 | head -n 2 > 4-password.txt

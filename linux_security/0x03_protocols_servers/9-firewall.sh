28ac3484b9889cf78ecad75a25aa1fe9cddddabe
JuanMartinMena <juanmartinmenac@gmail.com>
2025-06-22 13:58:04 -0400

    3 lines cscript corrected

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-iptables -P INPUT DROP
-iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -F; iptables -P INPUT DROP; iptables -A INPUT -p tcp --dport ssh -j ACCEPT
 iptables -A INPUT -j DROP
